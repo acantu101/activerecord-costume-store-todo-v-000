e38784d97b91a434945e29c73a7751ecbcd9fad7
alejandra cantu <alejandra.cantu13@gmail.com>
2020-03-04 03:07:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,9 +6,9 @@ def change
  create_table :costume_stores do |t|
    t.string :name
    t.string :location
-   t.integer :custume_inventory
-   t.integer :employees
-   t.string :active
+   t.integer :costume_inventory
+   t.integer :num_of_employees
+   t.boolean :still_in_business
    t.datetime :opening_time
    t.datetime :closing_time
 
